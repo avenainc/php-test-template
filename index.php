80ae4147c61b91c24db79cc0787838d0885968bf
germanztz <11229630+germanztz@users.noreply.github.com>
2026-03-06 12:54:01 +0100

    Add basic PHP HTML template with date display

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemple PHP</title>
+</head>
+<body>
+    <h1><?php echo date('d \d\e M \d\e\l Y') . " - Biba el desarrollo web!"; ?></h1>
+</body>
+</html>
